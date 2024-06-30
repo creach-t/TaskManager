00260fc49413e53646918c224bca8c816ca37598
creach-t <creach.t@gmail.com>
2024-06-30 20:08:14 +0200

    Reorganisation globale

--- start.js
+++ start.js
@@ -7,7 +7,29 @@ const djangoCommand = 'python3 manage.py runserver';
 const reactCommand = 'npm start';
 
 // Démarrer le serveur Django
-exec(djangoCommand);
+exec(djangoCommand, (error, stdout, stderr) => {
+    if (error) {
+        console.error(`Erreur lors du démarrage du serveur Django : ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.error(`Erreur lors du démarrage du serveur Django : ${stderr}`);
+        return;
+    }
+    console.log('Le serveur Django a démarré avec succès.');
+});
 
 // Démarrer le serveur React
-exec(reactCommand);
+exec(reactCommand, (error, stdout, stderr) => {
+    if (error) {
+        console.error(`Erreur lors du démarrage du serveur React : ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.error(`Erreur lors du démarrage du serveur React : ${stderr}`);
+        return;
+    }
+    console.log('Le serveur React a démarré avec succès.');
+});
+
+console.log('Vérification en cours de l\'application et des serveurs...');
