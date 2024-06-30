c7af79933bf9d9647fff1aebbe506a1052ede70d
creach-t <creach.t@gmail.com>
2024-06-30 20:07:33 +0200

    Reorganisation globale

--- install.js
+++
@@ -1,9 +0,0 @@
-const { exec } = require('child_process');
-
-const commandMakeMigration = 'python3 manage.py makemigration';
-const commandMigrate = 'python3 manage.py migrate';
-const commandeNpmI = 'npm i'
-
-exec(commandMakeMigration);
-exec(commandMigrate);
-exec(commandeNpmI);
